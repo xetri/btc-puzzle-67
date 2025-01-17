d46ae2ad48eab620ff49ae5c96fe8688e36c1dbb
xetri <xetrymebich@gmail.com>
2025-01-18 00:09:48 +0545

    divide and conquer

--- search/btc-random.py
+++ search/btc-random.py
@@ -1,15 +1,19 @@
+import threading
 import helpers as hs
 import random
 import config
 
+def search(i):
+    hex_key = hs.num_to_hex64(i)
+    assumed_addr = hs.pvkhex_to_address_compressed(hex_key)
+
+    if assumed_addr == config.comp_addr:
+        with open("./success-begin-" + str(i) + ".txt", "w+") as f: f.write(str(i) + ": " + assumed_addr + ": "  + hex_key)
+        print("Found bich")
+        exit()
+
+
 while True:
     nums = [random.randint(config.begin, config.Z + 1), random.randint(config.bR - 1, config.end)]
 
-    for n in nums:
-        hex_key = hs.num_to_hex64(n)
-        assumed_addr = hs.pvkhex_to_address_compressed(hex_key)
-
-        if assumed_addr == config.comp_addr:
-            with open("./success-random-" + str(n), "w+") as f: f.write(str(n) + ": " + assumed_addr + ": "  + hex_key)
-            print("Found bich")
-            exit()
+    for n in nums: threading.Thread(target=search, args=(n,)).start()
