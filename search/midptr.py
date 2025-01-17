ae27333fd9a7f7fcc7fe163238531bc31bbec3c2
xetri <xetrymebich@gmail.com>
2025-01-17 23:56:17 +0545

    gooder

--- search/midptr.py
+++ search/midptr.py
@@ -3,51 +3,27 @@ import config
 import threading
 
 def Scan(begin, end):
-    t1, t2 = threading.Thread(target=Down, args=(begin, end)), threading.Thread(target=Up, args=(begin, end))
-    t1.start()
-    t2.start()
+    threading.Thread(target=divide_and_conquer, args=(begin, end)).start()
 
-def Down(begin, end):
+def divide_and_conquer(begin, end):
     if end - begin <= 1:
         search(begin)
         return
 
     m = (begin + end) // 2
-    td1 = threading.Thread(target=Down, args=(begin, m))
-    td2 = threading.Thread(target=Down, args=(m, end))
-    tc1 = threading.Thread(target=ptrDecScan, args=(m, begin))
-    tc2 = threading.Thread(target=ptrIncScan, args=(m, end))
-    td1.start()
-    td2.start()
-    tc1.start()
-    tc2.start()
-
-def Up(begin, end):
-    if end - begin <= 1:
-        search(begin)
-        return
-
-    m = (begin + end) // 2
-    tu1 = threading.Thread(target=Up, args=(begin, m))
-    tu2 = threading.Thread(target=Up, args=(m, end))
-    tc1 = threading.Thread(target=ptrDecScan, args=(m, begin))
-    tc2 = threading.Thread(target=ptrIncScan, args=(m, end))
-    tu1.start()
-    tu2.start()
-    tc1.start()
-    tc2.start()
+    threads = [
+        threading.Thread(target=ptrDecScan, args=(m, begin)),
+        threading.Thread(target=ptrIncScan, args=(m + 1, end)),
+        threading.Thread(target=divide_and_conquer, args=(begin, m)),
+        threading.Thread(target=divide_and_conquer, args=(m + 1, end)),
+    ]
+    for t in threads: t.start()
 
 def ptrDecScan(n, delim):
-    i = n
-    while i > delim:
-        search(i)
-        i -= 1
+    for i in range(n, delim, -1): search(i)
 
 def ptrIncScan(n, delim):
-    i = n
-    while i < delim:
-        search(i)
-        i += 1
+    for i in range(n, delim + 1): search(i)
 
 def search(i):
     hex_key = hs.num_to_hex64(i)
